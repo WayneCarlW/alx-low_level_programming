680423e87939b54670f675d1993b0450eb6252d6
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-07 16:11:48 -0700

    Good

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\" - Dora Korpar
+			, 2015-10-19\n", stderr);
 	return (1);
 }
