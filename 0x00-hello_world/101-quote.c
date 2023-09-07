1b0fd04632ee5252031aaf1d8093a91b1c3a25ad
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-07 16:04:50 -0700

    Good

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Error(1)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
