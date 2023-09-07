8db494fd57e10958395e81ace59f098c7b7cf929
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-07 14:54:45 -0700

    Good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * Below is the main
+ *
  * Function of the program
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
