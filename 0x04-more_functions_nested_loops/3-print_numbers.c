565619a19b362a683fd447f6b02ae9d2d7a8d2f1
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-15 04:06:33 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i < 9; i++)
+	{
+		printf("%d", i);
+	}
+}
