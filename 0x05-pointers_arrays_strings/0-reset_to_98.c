b5bfb4a76d7b1c390b4d296fe9d0697130561662
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-18 05:15:17 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - Updates the value of a varible using a pointer
+ *
+ * @*n - pointer to an int variable
+ * 
+ * Return: NULL
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char);
+void reset_to_98(int *);
+#endif
