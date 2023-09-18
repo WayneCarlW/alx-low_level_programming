83f84aa6d5b7819383b2c65618aeb12bec58d186
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-18 07:20:32 -0700

    Good

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str: pointer to a character of a string variable
+ * Return: NULL
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int lent = strlen(str);
+
+	if (lent % 2 == 0)
+	{
+		n = lent / 2;
+
+		for (i = n; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+		putchar(10);
+	}
+	else
+	{
+		n = (lent - 1) / 2;
+		for (i = n; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+		putchar(10);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ void _puts(char *);
 void print_rev(char *);
 void rev_string(char *);
 void puts2(char *);
+void puts_half(char *);
 #endif
