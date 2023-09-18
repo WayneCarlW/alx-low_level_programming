4c2feeed381a5d68871d15d38058df73adcd7c54
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-18 05:51:22 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: pointer to the string
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int lent = 0;
+
+	while (*s != '\0')
+	{
+		lent++;
+		s++;
+	}
+	return (lent);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 int _putchar(char);
 void reset_to_98(int *);
 void swap_int(int *, int *);
+int _strlen(char *);
 #endif
