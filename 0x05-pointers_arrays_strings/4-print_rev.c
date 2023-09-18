01e4bcc8f368d0842875d246da3b4e3d00c1209f
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-18 06:23:13 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: pointer to a string character
+ * Return:NULL
+ */
+void print_rev(char *s)
+{
+	int lent;
+	int i;
+
+	lent = strlen(s);
+	for (i = lent - 1; i >=0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar(10);
+}
+	
