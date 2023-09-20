9a3cad7ef94017d453f1cac072186b2170c22dba
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-20 07:38:22 -0700

    CoNcatenate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * _strncat - appends n contents of src to dest
+ *
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of characters in src to be copied
+ * Return: Concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *result = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+	return (result);
+}
