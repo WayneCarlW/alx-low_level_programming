58d9a63c433f56fefe7c59ec84bb5cb5a773f3ba
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-20 20:29:02 +0300

    StriNg Copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 		i++;
@@ -21,5 +21,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
 }
