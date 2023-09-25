d923b2f23189ba55b2adb196ea1c11ccf0920565
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-25 14:10:01 -0700

    Cracked Password

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,10 @@
 int _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*)[]);
+void print_diagsums(int *, int);
+void set_string(char **, char *);
 #endif
