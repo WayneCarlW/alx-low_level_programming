104c8ac80982cf8608dae4ca8195c0e11f60bc35
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-28 01:40:08 +0300

    PrimeT

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,34 +1,34 @@
 #include "main.h"
 #include <math.h>
 /**
- * _isy_prime_number - Returns true if n is prime and otherwise false
+ * _is_prime_number - Returns true if n is prime and otherwise false
  *
  * @n: number to be tetsted
  * @div: divisibility checker
  * Return: 1 when n is prime and 0 when n is not prime
  */
-int _isy_prime_number(int n, int div)
+int _is_prime_number(int n, int div)
 {
+	int init = n / 2;
+	
 	if (n <= 1)
 		return (0);
-	if (div <= 1)
-	{
-		return (1);
-	}
-	if (n % div == 0)
-	{
-		return (0);
-	}
-	return (_isy_prime_number(n, div - 1));
-
+	return (is_prime(n, start));
 }
 /**
- * _is_prime_number - Checks if a number is prime
+ * _is_prime - Checks if a number is prime
  *
  * @n: number to be tested
+ * @init: Checkpoint
  * Return: 0 When false and 1 when true
  */
-int _is_prime_number(int n)
+int _is_prime(int n, int init)
 {
-	return (_isy_prime_number(n, (int)(sqrt(n))));
+	if (init <= 1)
+	{
+		return(1);
+	}
+	else if (n % init == 0)
+		return (0);
+	return (is_prime(n, start - 1));
 }
