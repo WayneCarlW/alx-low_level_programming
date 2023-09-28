90419f19b3ae0d8278af43adb716f0e4135e3450
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-28 20:17:07 +0300

    WildCmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,17 +8,13 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s2 == '\0')
 	{
-		return (1);
+		return (*s1 == '\0');
 	}
 	else if (*s2 == '*')
 	{
 		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-	}
-	else if (*s1 != *s2)
-	{
-		return (0);
 	}
 	else if (*s1 == *s2)
 	{
