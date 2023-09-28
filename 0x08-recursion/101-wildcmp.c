6c770672a4dd77625eff4582a0658e99725d23ac
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-28 03:25:21 +0300

    Comparewld

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s2 == '*')
 	{
-		return (0);
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
 	else if (*s1 != *s2)
 	{
