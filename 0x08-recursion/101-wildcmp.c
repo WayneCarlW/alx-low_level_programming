3f6a9085dcfff2b94a733eca27de0dc277b46521
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-28 03:09:40 +0300

    COmpared

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,10 +8,13 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s1 == *s2)
+	if (*s1 == '\0' && *s2 == '\0')
 	{
-		s1++;
-		s2++;
+		return (0);
 	}
-	return (*s1 - *s2);
+	else if (*s1 != *s2)
+	{
+		return (0);
+	}
+	return (wildcmp(s1 + 1, s2 + 1));
 }
