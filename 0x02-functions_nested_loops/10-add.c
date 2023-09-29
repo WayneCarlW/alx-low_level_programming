af638ab332acf826b5555770196d38328f58ba72
WayneCarlW <waynecarlgeno@gmail.com>
2023-09-29 05:57:33 -0700

    Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ *
+ * @n1: First integer
+ * @n2: Second integer
+ * Return: Sum of two numbers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table starting with 0
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	/*Not done*/
+}
