98e29eaa20dc6de07f8467ce720400e422d7b243
WayneCarlW <waynecarlgeno@gmail.com>
2023-10-02 04:34:43 -0700

    Argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - returns the name of the program
+ * @argc: number of commandline arguments
+ * @argv: Array of args of size argc
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
