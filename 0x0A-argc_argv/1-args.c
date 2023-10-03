4baaf59529d1a2efcdea20b879f57fd77240448c
WayneCarlW <waynecarlgeno@gmail.com>
2023-10-02 21:33:01 -0700

    Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments passed to it
+ *
+ * @argc: The numbe of arguments
+ * @argv: An array of the arguments of size argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
