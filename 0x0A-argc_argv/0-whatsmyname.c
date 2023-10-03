0ea90512ae98d5e965efcc68cc8e770c08fdb63c
WayneCarlW <waynecarlgeno@gmail.com>
2023-10-02 21:22:11 -0700

    Args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: Array of args of size argc
  * Return: 0 Success
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
