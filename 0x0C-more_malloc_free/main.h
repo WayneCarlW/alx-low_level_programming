b54cc89b909c36fdd898d0522c0a0be1816da81b
WayneCarlW <waynecarlgeno@gmail.com>
2023-10-19 22:05:01 +0300

    Delete 0x0C-more_malloc_free directory

--- 0x0C-more_malloc_free/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char);
-void *malloc_chaecked(unsigned int);
-char *string_nconcat(char *, char *, unsigned int);
-void *_calloc(unsigned int, unsigned int);
-int *array_range(int, int);
-void *_realloc(void *, unsigned int, unsigned int);
-#endif
