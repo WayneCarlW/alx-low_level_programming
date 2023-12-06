fdc3422c6ff16031ad74d3b30968206f0d06235d
WayneCarlW <waynecarlgeno@gmail.com>
2023-12-06 02:06:03 +0200

    summy

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,9 +7,10 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
 	unsigned int i;
 	int sum = 0;
-	va_list args;
+
 
 	if (n == 0)
 	{
@@ -18,7 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < n; ++i)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
