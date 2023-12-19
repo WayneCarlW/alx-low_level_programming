00ad65d83ccecf62233c651f4be6d6e3b3933009
WayneCarlW <waynecarlgeno@gmail.com>
2023-12-19 04:35:57 +0200

    fileio

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * append_text_to_file - function; appends_text at end of the file
+ *
+ * @filename:pointer:shows to the name of the file.
+ *
+ * @text_content: represent the string to be added at end of_file
+ *
+ * Return: filename is NULL -1 when the function fails.
+ *         The user to lack permision on missing file -1
+ *         Elsewhere return 1.
+ */
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	const char *f_name = filename;
+	char *t_c = text_content;
+	int i;
+	int j;
+	int len = 0;
+
+	if (f_name == NULL)
+		return (-1);
+
+	if (t_c != NULL)
+	{
+		for (len = 0; t_c[len];)
+			len++;
+	}
+
+	i = open(f_name, O_WRONLY | O_APPEND);
+	j = write(i, t_c, len);
+
+	if (i == -1 || j == -1)
+		return (-1);
+
+	close(i);
+
+	return (1);
+}
